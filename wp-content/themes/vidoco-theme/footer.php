be45bcb9c9d35419888a5a08e60c79926210c6b6
dienkim <trietnk01@gmail.com>
2019-06-19 17:10:20 +0700

    cập nhật

--- wp-content/themes/vidoco-theme/footer.php
+++ wp-content/themes/vidoco-theme/footer.php
@@ -5,6 +5,54 @@ Footer template
 
 */
 ?>
+<div class="container box-index">
+  <div class="row">
+    <div class="col">
+      <div class="nika">
+        <div class="owl-carousel-brand owl-carousel owl-theme owl-loaded">
+          <?php
+          for($i=0;$i<100;$i++) {
+            ?>
+            <div class="item">
+              <img src="<?php echo wp_get_upload_dir()["url"]."/logo-1.jpg"; ?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>">
+            </div>
+            <?php
+          }
+          ?>
+        </div>
+      </div>
+      <footer>
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="slogan-info">
+              <div>CÔNG TY TNHH PHÁT TRIỂN THƯƠNG MẠI THỌ AN</div>
+
+<div>Trụ sở: Bách Kim – Phú Kim – Thạch Thất – TP. Hà Nội</div>
+
+<div>VPGD: Số 14/322 Lê Trọng Tấn – Q. Thanh Xuân – TP. Hà Nội</div>
+
+<div>Email:&nbsp;bulongthanhren@gmail.com&nbsp;/&nbsp;thoanservice@gmail.com</div>
+
+<div>Website:&nbsp;www.bulongthanhren.vn&nbsp;/&nbsp;www.bulongthoan.com.vn</div>
+
+<div>Điện thoại:&nbsp;024.6326.3722&nbsp;/&nbsp;Hotline:&nbsp;0986 068 715&nbsp;–&nbsp;0982 83 1985</div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="fanpage-info">
+              <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="" data-height="200" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="map-box">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.7729502333677!2d106.6313720147495!3d10.828679992286109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175297c5dbf9479%3A0x809aeba3ce6bf8fb!2zSOG6u20gMTAyIFBoYW4gSHV5IMONY2gsIFBoxrDhu51uZyAxNSwgVMOibiBCw6xuaCwgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1560938858053!5m2!1svi!2s" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+</div>
 <!-- begin pan search -->
 <div class="pan_search">
   <div class="pan_close">
--- wp-content/themes/vidoco-theme/index.php
+++ wp-content/themes/vidoco-theme/index.php
@@ -61,21 +61,103 @@
                 </div>
             </div>
             <div class="col-lg-6">
-                <div class="dssp-box">
-                    <h2 class="dssp-h">Bu lông ốc vít</h2>
-                    <div class="main-product-box">
-                        <?php
-                        $k=0;
-                        for ($i=0; $i < 9; $i++) {
-                            if($k%3 == 0){
-                                echo '<div class="row">';
+                <?php
+                for ($v=0; $v < 3; $v++) {
+                    ?>
+                    <div class="dssp-box">
+                        <h2 class="dssp-h">Bu lông ốc vít</h2>
+                        <div class="main-product-box">
+                            <?php
+                            $k=0;
+                            for ($i=0; $i < 9; $i++) {
+                                if($k%3 == 0){
+                                    echo '<div class="row">';
+                                }
+                                ?>
+                                <div class="col-lg-4">
+                                    <div class="box-oc-vit-bulong-item">
+                                        <div class="box-oc-bu-img">
+                                            <a href="javascript:void(0);">
+                                                <div class="box-product-featured-img">
+                                                    <img src="<?php echo wp_upload_dir( null,true,false )["url"]."/bulong-304.jpg"; ?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>">
+                                                </div>
+                                                <div class="panel-top-to-bottom"></div>
+                                                <div class="panel-bottom-to-top"></div>
+                                                <div class="panel-link">
+                                                    <div class="panel-circle">
+                                                        <i class="fas fa-link"></i>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <h3 class="box-oc-bu-title"><a href="javascript:void(0);">Bu lông lục giác chìm đầu trụ DIN 912</a></h3>
+                                    </div>
+                                </div>
+                                <?php
+                                $k++;
+                                if($k%3 == 0 || $k==9){
+                                    echo '</div>';
+                                }
                             }
                             ?>
-                            <div class="col-lg-4">
-                                <div class="box-oc-vit-bulong-item">
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
+            <div class="col-lg-3">
+                <div class="support-online-box">
+                    <div class="ho-tro-tot">
+                        <div class="httt">
+                            <div class="ho-tro">Hỗ trợ</div>
+                            <div class="truc-tuyen">Trực tuyến</div>
+                        </div>
+                    </div>
+                    <div class="support-hotline">
+                        <div class="support-phone-icon"><img src="<?php echo get_template_directory_uri()."/assets/images/telephone.png"; ?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>"></div>
+                        <div class="support-phone-number"><a href="tel:0903600002">0903 600 002</a></div>
+                        <div class="clr"></div>
+                    </div>
+                    <div class="call-skype-viber-zalo">
+                        Call: Skype - Viber - Zalo
+                    </div>
+                    <div class="cara-call-skype-viber-zalo">
+                        <span><a href="skype:kimdien07?chat"><img src="<?php echo get_template_directory_uri()."/assets/images/skype.png";?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>"></a></span>
+                        <span><a href="javascript:void(0);"><img src="<?php echo get_template_directory_uri()."/assets/images/viber.png";?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>"></a></span>
+                        <span><a href="http://zalo.me/"><img src="<?php echo get_template_directory_uri()."/assets/images/zalo.png";?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>"></a></span>
+                    </div>
+                    <div class="support-email">
+                        <a href="mailto:nguyenbaotuoc@gmail.com">Email: nguyenbaotuoc@gmail.com</a>
+                    </div>
+                </div>
+
+                <div class="catalog-box">
+                    <a href="javascript:void(0);">
+                        <img src="<?php echo wp_get_upload_dir()["url"]."/catalog.jpg"; ?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>">
+                    </a>
+                </div>
+
+                <div class="box-right">
+                    <h2 class="bx-right-h">Quy trình sản xuất bu lông</h2>
+                    <div class="bx-right-wrapper">
+                        <iframe width="100%" height="200" src="https://www.youtube.com/embed/8OOCRpJSStE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div class="box-right">
+                    <h2 class="bx-right-h">Bài viết mới</h2>
+                    <div class="bx-right-wrapper">
+                        <?php
+                        for ($i=0; $i < 5; $i++) {
+                            ?>
+                            <div class="roda-mau-thiet-ke">
+                                <div class="mau-thiet-ke-left">
                                     <div class="box-oc-bu-img">
                                         <a href="javascript:void(0);">
-                                            <img src="<?php echo wp_upload_dir( null,true,false )["url"]."/bulong-304.jpg"; ?>" alt="">
+                                            <div class="box-product-featured-img">
+                                                <img src="<?php echo wp_upload_dir( null,true,false )["url"]."/bulong-304.jpg"; ?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>">
+                                            </div>
                                             <div class="panel-top-to-bottom"></div>
                                             <div class="panel-bottom-to-top"></div>
                                             <div class="panel-link">
@@ -85,53 +167,50 @@
                                             </div>
                                         </a>
                                     </div>
-                                    <h3 class="box-oc-bu-title"><a href="javascript:void(0);">Bu lông lục giác chìm đầu trụ DIN 912</a></h3>
                                 </div>
+                                <div class="mau-thiet-ke-right">
+                                    <h4 class="mau-thiet-ke-right-h4"><a href="javascript:void(0);">Phân phối bu lông ốc vít inox các loại giá rẻ, đúng tiêu chuẩn, chất lượng</a></h4>
+                                    <div class="margin-top-10 icon-heart">
+                                        <span><i class="far fa-clock"></i></span><span class="margin-left-5">12/09/2019</span><span class="margin-left-5"><i class="far fa-heart"></i></span>
+                                    </div>
+                                </div>
+                                <div class="clr"></div>
                             </div>
                             <?php
-                            $k++;
-                            if($k%3 == 0 || $k==9){
-                                echo '</div>';
-                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="box-right">
+                    <h2 class="bx-right-h">Sản phẩm mua nhiều</h2>
+                    <div class="bx-right-wrapper">
+                        <?php
+                        for ($i=0; $i < 5; $i++) {
+                            ?>
+                            <div class="box-oc-vit-bulong-item">
+                                <div class="box-oc-bu-img">
+                                    <a href="javascript:void(0);">
+                                        <div class="box-product-featured-img">
+                                                    <img src="<?php echo wp_upload_dir( null,true,false )["url"]."/bulong-304.jpg"; ?>" alt="<?php echo get_bloginfo( 'name', 'raw' ); ?>">
+                                                </div>
+                                        <div class="panel-top-to-bottom"></div>
+                                        <div class="panel-bottom-to-top"></div>
+                                        <div class="panel-link">
+                                            <div class="panel-circle">
+                                                <i class="fas fa-link"></i>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <h3 class="box-oc-bu-title"><a href="javascript:void(0);">Bu lông lục giác chìm đầu trụ DIN 912</a></h3>
+                                <div class="gia-lien-he"><span class="gia-1">Giá:</span><span class="lien-he-1">Liên hệ</span></div>
+                            </div>
+                            <?php
                         }
                         ?>
                     </div>
                 </div>
             </div>
-
-
-            <div class="col-lg-3">
-
-
-                <div class="support-online-box">
-                    <div class="ho-tro-tot">
-                        <div class="httt">
-                            <div class="ho-tro">Hỗ trợ</div>
-                            <div class="truc-tuyen">Trực tuyến</div>
-                        </div>
-                    </div>
-                    <div class="support-hotline">
-                        <div class="support-phone-icon"><img src="<?php echo get_template_directory_uri()."/assets/images/telephone.png"; ?>"></div>
-                        <div class="support-phone-number"><a href="tel:0903600002">0903 600 002</a></div>
-                        <div class="clr"></div>
-                    </div>
-                    <div class="call-skype-viber-zalo">
-                        Call: Skype - Viber - Zalo
-                    </div>
-                    <div class="cara-call-skype-viber-zalo">
-                        <span><a href="skype:kimdien07?chat"><img src="<?php echo get_template_directory_uri()."/assets/images/skype.png";?>"></a></span>
-                        <span><a href="javascript:void(0);"><img src="<?php echo get_template_directory_uri()."/assets/images/viber.png";?>"></a></span>
-                        <span><a href="http://zalo.me/"><img src="<?php echo get_template_directory_uri()."/assets/images/zalo.png";?>"></a></span>
-                    </div>
-                    <div class="support-email">
-                        <a href="mailto:nguyenbaotuoc@gmail.com">Email: nguyenbaotuoc@gmail.com</a>
-                    </div>
-                </div>
-
-
-            </div>
-
-
             <?php
             get_footer();
             ?>
